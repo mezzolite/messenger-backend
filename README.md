f078dced02e1b4a67043b4aaa54b9534e2afcd45
Mez Charney <mez@pana.com>
2022-10-15 11:11:46 -0600

    Initial server setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # messenger-backend
 A simple messenger backend with Node.js and Typescript
+
+Node version: 16.18.0
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, {Express} from 'express';
+import * as bodyParser from 'body-parser';
+
+const app: Express = express()
+const port = process.env.port || 3000;
+
+app.use(bodyParser.json());
+
+/** Error handling */
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  return res.status(404).json({
+      message: error.message
+  });
+});
+
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+})
