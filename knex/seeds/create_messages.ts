826be69d5645aa294243e935eaf8603eaf8580df
Mez Charney <mez@pana.com>
2022-10-15 19:58:02 -0600

    adds get by id and post messages routes

--- knex/seeds/create_messages.ts
+++ knex/seeds/create_messages.ts
@@ -7,16 +7,16 @@ export async function seed(knex: Knex): Promise<void> {
     const dateTooLongAgo = new Date(new Date().setDate(new Date().getDate() - 40))
     // Inserts seed entries
     await knex("messages").insert([
-        { id: 1, sender_id: 1, recipient_id: 2, message_text: "Come here often?" },
-        { id: 2, sender_id: 1, recipient_id: 2, message_text: "Let's go to Milk." },
-        { id: 3, sender_id: 1, recipient_id: 2, message_text: "What did we do last night? Where are my bows?" },
-        { id: 4, sender_id: 2, recipient_id: 1, message_text: "Lol goth" },
-        { id: 5, sender_id: 2, recipient_id: 1, message_text: "The girl in the bathroom needed them." },
-        { id: 6, sender_id: 1, recipient_id: 3, message_text: "They look prettier on you" },
-        { id: 7, sender_id: 3, recipient_id: 2, message_text: "have you heard that Neil Gaiman is on Tumblr?" },
-        { id: 8, sender_id: 3, recipient_id: 2, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
-        { id: 9, sender_id: 1, recipient_id: 2, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
-        { id: 10, sender_id: 2, recipient_id: 1, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
-        { id: 11, sender_id: 3, recipient_id: 1, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
+        { id: 1001, sender_id: 1, recipient_id: 2, message_text: "Come here often?" },
+        { id: 1002, sender_id: 1, recipient_id: 2, message_text: "Let's go to Milk." },
+        { id: 1003, sender_id: 1, recipient_id: 2, message_text: "What did we do last night? Where are my bows?" },
+        { id: 1004, sender_id: 2, recipient_id: 1, message_text: "Lol goth" },
+        { id: 1005, sender_id: 2, recipient_id: 1, message_text: "The girl in the bathroom needed them." },
+        { id: 1006, sender_id: 1, recipient_id: 3, message_text: "They look prettier on you" },
+        { id: 1007, sender_id: 3, recipient_id: 2, message_text: "have you heard that Neil Gaiman is on Tumblr?" },
+        { id: 1008, sender_id: 3, recipient_id: 2, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
+        { id: 1009, sender_id: 1, recipient_id: 2, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
+        { id: 1010, sender_id: 2, recipient_id: 1, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
+        { id: 1011, sender_id: 3, recipient_id: 1, message_text: "This message is tooooo old", created_at: dateTooLongAgo },
     ]);
 }
--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -18,22 +18,38 @@ const thirtyDaysAgo = new Date(new Date().setDate(todaysDate.getDate() - 30))
 export function getAllMessages(req: Request, res: Response) {
   return database('messages')
     .whereBetween('created_at', [thirtyDaysAgo, todaysDate] )
+    .limit(100)
     .then(results => res.send(results) )
 }
 
-export function getMessagesForRecipient() {
-  console.log('getMessagesForRecipient')
+export function getMessagesForRecipient(req: Request, res: Response) {
+  return database('messages')
+    .where('recipient_id', req.params.recipient_id)
+    .where('sender_id', req.body.sender_id)
+    .whereBetween('created_at', [thirtyDaysAgo, todaysDate])
+    .limit(100)
+    .then(results => res.send(results))
 }
 
-export function sendMessage() {
-  console.log('sendMessage')
+export function sendMessage(req: Request, res: Response) {
+  return database('messages')
+    .insert(
+      {
+        sender_id: req.body.sender_id,
+        recipient_id: req.body.recipient_id,
+        message_text: req.body.message_text
+      }
+    )
+    .then(() => res.status(200).json({
+      message: "Message sent"
+    }))
 }
 
 export default {
   getAllMessages,
   getMessagesForRecipient,
   sendMessage
-  }
+}
 
 
 
